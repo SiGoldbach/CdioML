955b4056d530a0d313ae01d601958db0d539f7c8
Valdemar Nielsen <v.nielsen@live.dk>
2023-03-01 10:54:15 +0100

    fixed errors because python is wierd

--- robotInfo.py
+++ robotInfo.py
@@ -1,20 +1,21 @@
 class GameInfo:
-
-y, yr, ysm, ylm = int
-x, xr, xsm, xlm = int
-robotLocation = [xr, yr]
-ballsLocation = [x, y]
-robotStartLocation = [10, 1]
-gameWon = 0
-ballsDelivered = 0
-ballsCollected = 0
-goalLocationSmall = [xsm, ysm]
-goalLocationLarge = [xlm, ylm]
-totalBalls = len(ballsLocation)
-ballsRemaining = int(totalBalls - ballsCollected)
+    y, yr, ysm, ylm = int
+    x, xr, xsm, xlm = int
+    robotLocation = [xr, yr]
+    ballsLocation = [x, y]
+    robotStartLocation = [10, 1]
+    gameWon = 0
+    ballsDelivered = 0
+    ballsCollected = 0
+    goalLocationSmall = [xsm, ysm]
+    goalLocationLarge = [xlm, ylm]
+    totalBalls = len(ballsLocation)
+    ballsRemaining = int(totalBalls - ballsCollected)
 
 # created fake scenario for the robot
 Game1 = GameInfo()
+y, yr, ysm, ylm = int
+x, xr, xsm, xlm = int
 Game1.ysm = 10
 Game1.xsm = 1
 Game1.xlm = 20
@@ -27,6 +28,7 @@ Game1.ballsLocation = [[2, 4], [6, 3], [8, 8], [8, 3], [10, 10], [20, 20], [1, 9
 Game1.totalBalls = len(Game1.ballsLocation)
 Game1.ballsRemaining = int(Game1.totalBalls - Game1.ballsCollected)
 
+gameWon = 0
 if gameWon == 0:
 
     if Game1.ballsRemaining == 0:
